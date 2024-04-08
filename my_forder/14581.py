25d8669a196c4cdc254d298309f8544f7b01c1a8
swubio <dud8362@swu.ac.kr>
2024-04-08 12:46:45 +0900

    04.03

---
+++ my_forder/14581.py
@@ -0,0 +1,3 @@
+id = input()
+
+print(":fan::fan::fan:\n:fan::" + id + "::fan:\n:fan::fan::fan:")
---
+++ my_forder/2439.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+
+for i in range(N):
+    a = "*" * (i + 1)
+    print(a.rjust(N))
+
+# 뭐가 문제노,,
